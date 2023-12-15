aea343f9823778edbdfd246763fde796afd972f5
Tim Knops <timknops22@gmail.com>
2023-12-15 20:01:39 +0100

    Removing unused dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useRef } from "react";
-import html2pdf from "html2pdf.js";
 import Resume from "./components/resume/Resume.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import sampleData from "./sample_data.js";
--- src/components/utils/Navbar.jsx
+++ src/components/utils/Navbar.jsx
@@ -1,4 +1,3 @@
-import Button from "./button/Button";
 import SampleDataButtons from "./button/SampleDataButtons";
 
 /**
